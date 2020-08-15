2a2632498ef975ba68a39b4c1600643bfbc193bb
Adrian Norte <adrian@bashlines.com>
2020-08-16 00:00:36 +0200

    Adding vue to this

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { RegistryController } from './controllers/registry.controller';
 import { AppService } from './app.service';
 import { RegistryService } from './services/registry.service';
+import { ServeStaticModule} from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client/dist'),
+    }),
+  ],
   controllers: [AppController, RegistryController],
   providers: [AppService, RegistryService],
 })
